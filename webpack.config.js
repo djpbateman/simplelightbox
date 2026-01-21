d828f12568d9e6230608d86c364f4466d675886c
Daniel Bateman <djpbateman@gmail.com>
2026-01-21 22:43:26 +0000

    Added webpack
    Restructure and webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+    mode: 'production',
+    entry: {
+        main: './src/index.js',
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
